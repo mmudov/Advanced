7732aed2836cf49af627def416272a0eef9f3210
mmudov <milen.mudov@gmail.com>
2024-12-03 11:33:07 +0200

    Sixth commit:
    	- Using built-in components to create HTML forms
    	- Validating form data
    	- Responding to form events
    	- Using Entity Framework Core with Blazor components
    	- Performing CRUD operations
    	- Extending Blazor

--- obj/Debug/net9.0/Advanced.AssemblyInfo.cs
+++ obj/Debug/net9.0/Advanced.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Advanced")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e6ce5d9b52064790653d4ab135e2168befe4f06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a51f388273fb47020181e5a49a538b19e78a92d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Advanced")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Advanced")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
